c4b0049f242a82d16c3bd4f6a8912054b1a5279a
atherman2 <alexandre.t.herman@gmail.com>
2025-12-03 12:52:21 -0300

    Add Expression classes and constructors

--- include/ast.hpp
+++ include/ast.hpp
@@ -8,6 +8,11 @@ using namespace std;
 
 enum class VarType { INTEGER, BOOLEAN };
 enum class RoutType { PROCEDURE, FUNCTION };
+enum class Op {
+    E, NE, L, LE, G, GE,
+    ADD, SUB, MUL, DIV,
+    OR, AND, NOT
+};
 
 class No {
     public:
@@ -38,6 +43,57 @@ class NoSubroutine : public No {
 
         NoSubroutine(string identifier, RoutType rout_type, VarType return_type, vector<shared_ptr<NoDeclaration>> formal_param, vector<shared_ptr<NoDeclaration>> decl_section);
         ~NoSubroutine();
+};
+
+class NoExpression : public No {
+    public:
+        VarType inferred_type;
+
+        ~NoExpression();
+};
+
+class NoUnaryExpr : public NoExpression {
+    public:
+        shared_ptr<NoExpression> operand;
+        Op op;
+
+        NoUnaryExpr(shared_ptr<NoExpression> operand, Op op);
+        ~NoUnaryExpr();
+};
+
+class NoBinExpr : public NoExpression {
+    public:
+        shared_ptr<NoExpression> left, right;
+        Op op;
+
+        NoBinExpr(shared_ptr<NoExpression> left, shared_ptr<NoExpression> right, Op op);
+        ~NoBinExpr();
+};
+
+class NoVarExpr : public NoExpression {
+    public:
+        string identifier;
+
+        NoVarExpr(strin identifier);
+        ~NoVarExpr();
+};
+
+class NoLiteralExpr : public NoExpression {
+    public:
+        int num;
+        bool logic;
+        VarType type;
+
+        NoLiteralExpr(int num);
+        NoLiteralExpr(bool logic);
+};
+
+class NoCallExpr : public NoExpression {
+    public:
+        string identifier;
+        vector<shared_ptr<NoExpression>> expression_list;
+
+        NoCallExpr(string identifier);
 };
 
 class NoProgram : public No {
